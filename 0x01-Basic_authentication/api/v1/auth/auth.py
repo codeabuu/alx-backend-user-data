98e0868252ddbe6b7da21e07c1a7c62d286df75e
codeabuu <abdulngui718@gmail.com>
2024-02-12 22:17:24 +0000

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+class Auth:
+    '''class method'''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''public method for our class'''
+
+    def authorization_header(self, request=None) -> str:
+        '''method to return none rqs'''
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''method for current user'''
+
+        return None
