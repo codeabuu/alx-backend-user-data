945fd86bfc9c2dc63b3bcf49218a1bdc44bf2286
codeabuu <abdulngui718@gmail.com>
2024-02-14 22:21:14 +0000

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,6 +44,7 @@ def before_request():
                          '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, excluded_list):
+            cookie = auth.session_cookie(request)
             if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
