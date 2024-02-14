72c20ba214ca13c4419a351db116693e6ced52a9
codeabuu <abdulngui718@gmail.com>
2024-02-14 21:25:47 +0000

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for authentication using Session auth
+"""
+
+
+from .auth import Auth
+
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    '''classs session'''
+    pass
