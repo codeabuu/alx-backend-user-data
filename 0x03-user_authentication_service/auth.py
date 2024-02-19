6dc42c5568e9db0da3b87363063a3404d0fa1f27
codeabuu <abdulngui718@gmail.com>
2024-02-19 21:52:08 +0000

    Hashing passwords

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,11 @@
 '''auth py methods'''
 
 
-def _hash_password(self, password: str) -> bytes:
+import bcrypt
 
 
+def _hash_password(password: str) -> bytes:
+    '''hashimg pwd method'''
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
