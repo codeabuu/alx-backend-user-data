7c385616e2aa012c354104c18760e63a86eaa8ba
codeabuu <abdulngui718@gmail.com>
2024-02-20 20:49:38 +0000

    app update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,16 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profileb', methods=['GET'])
+def profile() -> str:
+    '''check user if exist'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
