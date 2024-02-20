6adbbb74d3c4c9f4cd89c57b695cdeacaccd81e9
codeabuu <abdulngui718@gmail.com>
2024-02-20 20:17:37 +0000

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
-from user import Base,User
+from user import Base, User
 
 
 class DB:
